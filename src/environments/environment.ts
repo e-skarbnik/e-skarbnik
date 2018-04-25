5065f794143e4364854d21462ee8dda8436dcb77
deepbreathnoob <kuciel.pawel@gmail.com>
2018-04-25 22:32:10 +0200

    member model, auth and member services, added firebase config test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  fireBaseConfig: {
+    apiKey: 'AIzaSyDeJXKSIRHhCM5og6Zc77Py96GAXzJSh3Y',
+    authDomain: 'test-bfceb.firebaseapp.com',
+    databaseURL: 'https://test-bfceb.firebaseio.com',
+    projectId: 'test-bfceb',
+    storageBucket: 'test-bfceb.appspot.com',
+    messagingSenderId: '169224143007'
+  }
 };
