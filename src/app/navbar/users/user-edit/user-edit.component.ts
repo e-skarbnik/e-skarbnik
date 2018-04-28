154d9e1deab8adc13b72a272437d2c9aa8db733d
deepbreathnoob <kuciel.pawel@gmail.com>
2018-04-28 22:13:27 +0200

    member proofile update completed

--- src/app/navbar/users/user-edit/user-edit.component.ts
+++ src/app/navbar/users/user-edit/user-edit.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from '../../../services/auth.service';
 import { MemberService } from '../../../services/member.service';
 import { Member } from '../../../models/member.model';
 import { ActivatedRoute, Params } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-user-edit',
@@ -17,11 +18,26 @@ export class UserEditComponent implements OnInit {
     private memberService: MemberService,
     private authService: AuthService,
     private route: ActivatedRoute,
-  ) { }
-
-  ngOnInit() {
+  ) {
     this.route.params.switchMap((params: Params) => this.memberService.getMemberByUID(params['key']))
     .subscribe(user => this.member = user);
+
+  }
+
+  ngOnInit() {
+
+  }
+  updateMember(tableData: NgForm) {
+    const member: Member = {
+      name: tableData.value.name,
+      surname: tableData.value.surname,
+      email: tableData.value.email,
+      paymaster: tableData.value.paymaster,
+      admin: tableData.value.admin,
+      key: tableData.value.key
+    };
+    //console.log(member);
+    this.memberService.updateMember(this.member);
   }
 
 }
