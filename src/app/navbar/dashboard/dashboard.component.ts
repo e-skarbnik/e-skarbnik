2d873e998dcf8dda31cabfe428e5aec4a26ec8d0
Klapaucius4 <klapaucius.mk@gmail.com>
2018-06-18 00:02:01 +0200

    drone zmiany przy dashboardzie

--- src/app/navbar/dashboard/dashboard.component.ts
+++ src/app/navbar/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { MemberService } from '../../services/member.service';
-import { ExchangeratesService } from '../../services/exchangerates.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,16 +11,11 @@ export class DashboardComponent implements OnInit {
   numberOfSchools: number;
   numberOfEvents: number;
 
-  exchangeRateOfUSD: number;
-  exchangeRateOfEUR: number;
-
-  constructor(private memberService: MemberService, private exchangeratesService: ExchangeratesService) {
-    this.numberOfUsers = memberService.getNumberOfMembers();
-    this.numberOfSchools = 23;
-    this.numberOfEvents = 435;
-
-    this.exchangeRateOfUSD = exchangeratesService.getExchangeRateByPLN('USD');
-    this.exchangeRateOfEUR = exchangeratesService.getExchangeRateByPLN('EUR');
+  constructor(private memberService: MemberService) {
+    // this.numberOfUsers = memberService.getNumberOfMembers();
+    this.numberOfUsers = 14;
+    this.numberOfSchools = 7;
+    this.numberOfEvents = 5;
   }
 
   ngOnInit() {
